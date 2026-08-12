01851b1f51aa69e7bc9da55de25cfba88bd0413d
kiw0n <heeone2001@naver.com>
2026-08-12 21:51:39 +0900

    chore : 스프링부트 환경 설정

---
+++ src/test/java/kr/deepsync/wellness/WellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.deepsync.wellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
